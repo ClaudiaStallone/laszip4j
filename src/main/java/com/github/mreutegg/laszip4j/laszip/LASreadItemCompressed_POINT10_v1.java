0a0429e6be2dc21d353747add47022d19ded1357
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-07-06 18:29:17 +0200

    id: 82, 20 minuti
    La risoluzione presente in questo commit risolve le issues con id 82, in quanto richiede di ridurre la complessità computazionale. il tempo stimato è di 20 minuti.

--- src/main/java/com/github/mreutegg/laszip4j/laszip/LASreadItemCompressed_POINT10_v1.java
+++ src/main/java/com/github/mreutegg/laszip4j/laszip/LASreadItemCompressed_POINT10_v1.java
@@ -81,127 +81,70 @@ public class LASreadItemCompressed_POINT10_v1 extends LASreadItemCompressed {
     }
 
     @Override
-    public PointDataRecord read(int notUsed) {
+   public PointDataRecord read(int notUsed) {
+    int median_x = findMedian(last_x_diff);
+    int median_y = findMedian(last_y_diff);
 
-        // find median difference for x and y from 3 preceding differences
-        int median_x;
-        if (last_x_diff[0] < last_x_diff[1])
-        {
-            if (last_x_diff[1] < last_x_diff[2])
-                median_x = last_x_diff[1];
-            else if (last_x_diff[0] < last_x_diff[2])
-                median_x = last_x_diff[2];
-            else
-                median_x = last_x_diff[0];
-        }
-        else
-        {
-            if (last_x_diff[0] < last_x_diff[2])
-                median_x = last_x_diff[0];
-            else if (last_x_diff[1] < last_x_diff[2])
-                median_x = last_x_diff[2];
-            else
-                median_x = last_x_diff[1];
+    int x_diff = ic_dx.decompress(median_x);
+    last_item.X += x_diff;
+
+    int k_bits = ic_dx.getK();
+    int y_diff = ic_dy.decompress(median_y, Math.min(k_bits, 19));
+    last_item.Y += y_diff;
+    k_bits = (k_bits + ic_dy.getK()) / 2;
+    last_item.Z = ic_z.decompress((int) last_item.Z, Math.min(k_bits, 19));
+
+    int changed_values = dec.decodeSymbol(m_changed_values);
+
+    if (changed_values != 0) {
+        if ((changed_values & 32) != 0) {
+            last_item.Intensity = (char) ic_intensity.decompress(last_item.Intensity);
         }
 
-        int median_y;
-        if (last_y_diff[0] < last_y_diff[1])
-        {
-            if (last_y_diff[1] < last_y_diff[2])
-                median_y = last_y_diff[1];
-            else if (last_y_diff[0] < last_y_diff[2])
-                median_y = last_y_diff[2];
-            else
-                median_y = last_y_diff[0];
-        }
-        else
-        {
-            if (last_y_diff[0] < last_y_diff[2])
-                median_y = last_y_diff[0];
-            else if (last_y_diff[1] < last_y_diff[2])
-                median_y = last_y_diff[2];
-            else
-                median_y = last_y_diff[1];
+        if ((changed_values & 16) != 0) {
+            initializeSymbolModelIfNull(m_bit_byte, last_item.Flags, 256);
+            last_item.Flags = (byte) dec.decodeSymbol(m_bit_byte[last_item.Flags]);
         }
 
-        // decompress x y z coordinates
-        int x_diff = ic_dx.decompress(median_x);
-        last_item.X += x_diff;
-        // we use the number k of bits corrector bits to switch contexts
-        int k_bits = ic_dx.getK(); // unsigned
-        int y_diff = ic_dy.decompress(median_y, (k_bits < 19 ? k_bits : 19));
-        last_item.Y += y_diff;
-        k_bits = (k_bits + ic_dy.getK())/2;
-        last_item.Z = ic_z.decompress((int)last_item.Z, (k_bits < 19 ? k_bits : 19));
-
-        // decompress which other values have changed
-        int changed_values = dec.decodeSymbol(m_changed_values);
-
-        if (changed_values != 0)
-        {
-            // decompress the intensity if it has changed
-            if ((changed_values & 32) != 0)
-            {
-                last_item.Intensity = (char) ic_intensity.decompress(last_item.Intensity);
-            }
-
-            // decompress the flags, ... if has changed
-            if ((changed_values & 16) != 0)
-            {
-                if (m_bit_byte[last_item.Flags] == null)
-                {
-                    m_bit_byte[last_item.Flags] = dec.createSymbolModel(256);
-                    dec.initSymbolModel(m_bit_byte[last_item.Flags]);
-                }
-                last_item.Flags = (byte) dec.decodeSymbol(m_bit_byte[last_item.Flags]);
-            }
-
-            // decompress the classification ... if it has changed
-            if ((changed_values & 8) != 0)
-            {
-                if (m_classification[last_item.Classification] == null)
-                {
-                    m_classification[last_item.Classification] = dec.createSymbolModel(256);
-                    dec.initSymbolModel(m_classification[last_item.Classification]);
-                }
-                last_item.Classification = (short) dec.decodeSymbol(m_classification[last_item.Classification]);
-            }
-
-            // decompress the scan_angle_rank ... if it has changed
-            if ((changed_values & 4) != 0)
-            {
-                last_item.ScanAngleRank = (byte) ic_scan_angle_rank.decompress(last_item.ScanAngleRank, k_bits < 3 ? 1 : 0);
-            }
-
-            // decompress the user_data ... if it has changed
-            if ((changed_values & 2) != 0)
-            {
-                if (m_user_data[last_item.UserData] == null)
-                {
-                    m_user_data[last_item.UserData] = dec.createSymbolModel(256);
-                    dec.initSymbolModel(m_user_data[last_item.UserData]);
-                }
-                last_item.UserData = (short) dec.decodeSymbol(m_user_data[last_item.UserData]);
-            }
-
-            // decompress the point_source_ID ... if it has changed
-            if ((changed_values & 1) != 0)
-            {
-                last_item.PointSourceID = (char)ic_point_source_ID.decompress(last_item.PointSourceID);
-            }
+        if ((changed_values & 8) != 0) {
+            initializeSymbolModelIfNull(m_classification, last_item.Classification, 256);
+            last_item.Classification = (short) dec.decodeSymbol(m_classification[last_item.Classification]);
         }
 
-        // record the difference
-        last_x_diff[last_incr] = x_diff;
-        last_y_diff[last_incr] = y_diff;
-        last_incr++;
-        if (last_incr > 2) last_incr = 0;
+        if ((changed_values & 4) != 0) {
+            last_item.ScanAngleRank = (byte) ic_scan_angle_rank.decompress(last_item.ScanAngleRank, Math.min(k_bits, 3));
+        }
 
-        // copy the last point
-        PointDataRecordPoint10 result = new PointDataRecordPoint10(last_item);
+        if ((changed_values & 2) != 0) {
+            initializeSymbolModelIfNull(m_user_data, last_item.UserData, 256);
+            last_item.UserData = (short) dec.decodeSymbol(m_user_data[last_item.UserData]);
+        }
 
-        return result;
+        if ((changed_values & 1) != 0) {
+            last_item.PointSourceID = (char) ic_point_source_ID.decompress(last_item.PointSourceID);
+        }
     }
+
+    last_x_diff[last_incr] = x_diff;
+    last_y_diff[last_incr] = y_diff;
+    last_incr = (last_incr + 1) % 3;
+
+    PointDataRecordPoint10 result = new PointDataRecordPoint10(last_item);
+    return result;
+}
+
+private int findMedian(int[] array) {
+    Arrays.sort(array);
+    return array[1];
+}
+
+private void initializeSymbolModelIfNull(Object[] array, int index, int size) {
+    if (array[index] == null) {
+        array[index] = dec.createSymbolModel(size);
+        dec.initSymbolModel(array[index]);
+    }
+}
+
 
     @Override	
     public boolean chunk_sizes() {	
