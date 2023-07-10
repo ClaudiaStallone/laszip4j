7a57ab290015a257254f7be4070c36c28de365f2
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-07-10 16:53:07 +0200

    id: 2134 ; tempo 5 minuti
    La risoluzione presente in questo commit risolve le issues con id 2134 , richiede la rimozione del parametro del metodo poichè non utilizzato. il tempo stimato è di 5 minuti

--- src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderBIL.java
+++ src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderBIL.java
@@ -39,7 +39,7 @@ public class LASreaderBIL extends LASreader {
         return false;
     }
 
-    public boolean open(String file_name) {
+    public boolean open() {
         return false;
     }
 
