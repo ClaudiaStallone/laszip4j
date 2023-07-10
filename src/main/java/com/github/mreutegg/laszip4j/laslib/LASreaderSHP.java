8b99171953fccf730b844972bcdae252d502c610
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-07-10 17:13:50 +0200

    id: 2302; tempo 5 minuti
    La risoluzione presente in questo commit risolve le issues con id 2302, richiede la rimozione del parametro del metodo poichè non utilizzato. il tempo stimato è di 5 minuti

--- src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderSHP.java
+++ src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderSHP.java
@@ -39,7 +39,7 @@ public class LASreaderSHP extends LASreader {
         return false;
     }
 
-    public boolean open(String file_name) {
+    public boolean open() {
         return false;
     }
 
