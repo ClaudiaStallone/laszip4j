56faac930d4ad3ebabfb984682216e2f8fddbf8d
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-07-11 12:31:56 +0200

    id: 2152 ; 5 minuti
    La issue con id 2152 è stata risolta; ho aggiunto un commento annidato che spieghi perché questo metodo è vuoto. il tempo stimato è di 5 minuti

--- src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderBuffered.java
+++ src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderBuffered.java
@@ -41,9 +41,10 @@ public class LASreaderBuffered extends LASreader {
 
 
     public void set_buffer_size(float buffer_size) {
+        throw new UnsupportedOperationException("close(boolean) method is not supported");
 
     }
-
+  
     public void set_scale_factor(double[] scale_factor) {
 
     }
