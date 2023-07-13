999a4fb8528b8f2bed3bb564925abd00dc81bf76
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-07-13 10:20:30 +0200

    id: 2307 a 2347 ; tempo 2/ 5 minuti
    La risoluzione presente in questo commit vede la ridenominazione delle issues 2307 a 2345 in un tempo di 2 minuti a issues
    e della rimozione dei paramentri della 2347 in 5 minuti

--- src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderTXT.java
+++ src/main/java/com/github/mreutegg/laszip4j/laslib/LASreaderTXT.java
@@ -22,7 +22,7 @@ public class LASreaderTXT extends LASreader {
     }
 
     @Override
-    public boolean seek(long p_index) {
+    public boolean seek_K(long p_index) {
         return false;
     }
 
@@ -32,7 +32,7 @@ public class LASreaderTXT extends LASreader {
     }
 
     @Override
-    public void close(boolean close_stream) {
+    public void close_2(boolean close_stream) {
 
     }
 
@@ -41,47 +41,47 @@ public class LASreaderTXT extends LASreader {
         return false;
     }
 
-    public void set_pts(boolean pts) {
+    public void set_pts_E(boolean pts) {
 
     }
 
-    public void set_ptx(boolean ptx) {
+    public void set_ptX(boolean ptx) {
 
     }
 
-    public void set_translate_intensity(float translate_intensity) {
+    public void set_translate_intensitY(float translate_intensity) {
 
     }
 
-    public void set_scale_intensity(float scale_intensity) {
+    public void Set_scale_intensity(float scale_intensity) {
 
     }
 
-    public void set_translate_scan_angle(float translate_scan_angle) {
+    public void set_transLate_scan_angle(float translate_scan_angle) {
 
     }
 
-    public void set_scale_scan_angle(float scale_scan_angle) {
+    public void set_scale_Scan_angle(float scale_scan_angle) {
 
     }
 
-    public void set_scale_factor(double[] scale_factor) {
+    public void SET_scale_factor(double[] scale_factor) {
 
     }
 
-    public void set_offset(double[] offset) {
+    public void set_offseTT(double[] offset) {
 
     }
 
-    public void add_attribute(int attribute_data_type, String attribute_name, String attribute_description, double attribute_scale, double attribute_offset, double attribute_pre_scale, double attribute_pre_offset) {
+    public void add_attribuTE(int attribute_data_type, String attribute_name, String attribute_description, double attribute_scale, double attribute_offset, double attribute_pre_scale, double attribute_pre_offset) {
 
     }
 
-    public boolean open() {
+    public boolean open_$() {
         return false;
     }
 
-    public boolean open(InputStream in, int i, String parse_string, int skip_lines, boolean b) {
+    public boolean open_2() {
         return false;
     }
 }
