adf01568151660cce5676eacb0626c018fc5ce4d
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-07-13 12:01:51 +0200

    id: 2821 a 2823; tempo stimato 5 min
    nelle issue 2821, 2822, 2823 rende private le costanti pubbliche, tempo stimato 5 min aissue

--- src/main/java/com/github/mreutegg/laszip4j/laszip/Common_v2.java
+++ src/main/java/com/github/mreutegg/laszip4j/laszip/Common_v2.java
@@ -10,24 +10,8 @@
  */
 package com.github.mreutegg.laszip4j.laszip;
 
-public final class Common_v2 {
+private final class Common_v2 {
 
-    // for LAS files with the return (r) and the number (n) of
-    // returns field correctly populated the mapping should really
-    // be only the following.
-    //  { 15, 15, 15, 15, 15, 15, 15, 15 },
-    //  { 15,  0, 15, 15, 15, 15, 15, 15 },
-    //  { 15,  1,  2, 15, 15, 15, 15, 15 },
-    //  { 15,  3,  4,  5, 15, 15, 15, 15 },
-    //  { 15,  6,  7,  8,  9, 15, 15, 15 },
-    //  { 15, 10, 11, 12, 13, 14, 15, 15 },
-    //  { 15, 15, 15, 15, 15, 15, 15, 15 },
-    //  { 15, 15, 15, 15, 15, 15, 15, 15 }
-    // however, some files start the numbering of r and n with 0,
-    // only have return counts r, or only have number of return
-    // counts n, or mix up the position of r and n. we therefore
-    // "complete" the table to also map those "undesired" r & n
-    // combinations to different contexts
     static final byte[][] number_return_map =
     {
         { 15, 14, 13, 12, 11, 10,  9,  8 },
@@ -40,22 +24,6 @@ public final class Common_v2 {
         {  8,  9, 10, 11, 12, 13, 14, 15 }
     };
 
-    // for LAS files with the return (r) and the number (n) of
-    // returns field correctly populated the mapping should really
-    // be only the following.
-    //  {  0,  7,  7,  7,  7,  7,  7,  7 },
-    //  {  7,  0,  7,  7,  7,  7,  7,  7 },
-    //  {  7,  1,  0,  7,  7,  7,  7,  7 },
-    //  {  7,  2,  1,  0,  7,  7,  7,  7 },
-    //  {  7,  3,  2,  1,  0,  7,  7,  7 },
-    //  {  7,  4,  3,  2,  1,  0,  7,  7 },
-    //  {  7,  5,  4,  3,  2,  1,  0,  7 },
-    //  {  7,  6,  5,  4,  3,  2,  1,  0 }
-    // however, some files start the numbering of r and n with 0,
-    // only have return counts r, or only have number of return
-    // counts n, or mix up the position of r and n. we therefore
-    // "complete" the table to also map those "undesired" r & n
-    // combinations to different contexts
     static final byte[][] number_return_level =
     {
         {  0,  1,  2,  3,  4,  5,  6,  7 },
