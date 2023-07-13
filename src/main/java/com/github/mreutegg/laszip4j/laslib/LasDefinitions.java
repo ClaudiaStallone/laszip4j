7ae72555ea466aeec3a9c74be6923fa7e77cd29f
ClaudiaStallone <95074390+ClaudiaStallone@users.noreply.github.com>
2023-07-13 11:20:47 +0200

    id :2636 ; tempo 5 minuti
    nella issue id 2636 ho aggiunto un costruttore privato per nascondere quello pubblico , il tempo stimato è di 5 minuti

--- src/main/java/com/github/mreutegg/laszip4j/laslib/LasDefinitions.java
+++ src/main/java/com/github/mreutegg/laszip4j/laslib/LasDefinitions.java
@@ -11,28 +11,32 @@
 package com.github.mreutegg.laszip4j.laslib;
 
 public class LasDefinitions {
-
     public static final int LAS_TOOLS_VERSION = 220310;
 
     public static final int LAS_TOOLS_FORMAT_DEFAULT = 0;
-    public static final int LAS_TOOLS_FORMAT_LAS     = 1;
-    public static final int LAS_TOOLS_FORMAT_LAZ     = 2;
-    public static final int LAS_TOOLS_FORMAT_BIN     = 3;
-    public static final int LAS_TOOLS_FORMAT_QFIT    = 4;
-    public static final int LAS_TOOLS_FORMAT_VRML    = 5;
-    public static final int LAS_TOOLS_FORMAT_TXT     = 6;
-    public static final int LAS_TOOLS_FORMAT_SHP     = 7;
-    public static final int LAS_TOOLS_FORMAT_ASC     = 8;
-    public static final int LAS_TOOLS_FORMAT_BIL     = 9;
-    public static final int LAS_TOOLS_FORMAT_FLT     = 10;
-    public static final int LAS_TOOLS_FORMAT_DTM     = 11;
+    public static final int LAS_TOOLS_FORMAT_LAS = 1;
+    public static final int LAS_TOOLS_FORMAT_LAZ = 2;
+    public static final int LAS_TOOLS_FORMAT_BIN = 3;
+    public static final int LAS_TOOLS_FORMAT_QFIT = 4;
+    public static final int LAS_TOOLS_FORMAT_VRML = 5;
+    public static final int LAS_TOOLS_FORMAT_TXT = 6;
+    public static final int LAS_TOOLS_FORMAT_SHP = 7;
+    public static final int LAS_TOOLS_FORMAT_ASC = 8;
+    public static final int LAS_TOOLS_FORMAT_BIL = 9;
+    public static final int LAS_TOOLS_FORMAT_FLT = 10;
+    public static final int LAS_TOOLS_FORMAT_DTM = 11;
 
     public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_GPS_TIME_TYPE = 0;
-    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_WDP_INTERNAL  = 1;
-    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_WDP_EXTERNAL  = 2;
-    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_SYNTHETIC     = 3;
-    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_OGC_WKT_CRS   = 4;
+    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_WDP_INTERNAL = 1;
+    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_WDP_EXTERNAL = 2;
+    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_SYNTHETIC = 3;
+    public static final int LAS_TOOLS_GLOBAL_ENCODING_BIT_OGC_WKT_CRS = 4;
 
-    public static final int LAS_TOOLS_IO_IBUFFER_SIZE   = 262144;
-    public static final int LAS_TOOLS_IO_OBUFFER_SIZE   = 262144;
+    public static final int LAS_TOOLS_IO_IBUFFER_SIZE = 262144;
+    public static final int LAS_TOOLS_IO_OBUFFER_SIZE = 262144;
+
+    // Private constructor to hide the implicit public one
+    private LasDefinitions() {
+        throw new AssertionError("LasDefinitions class should not be instantiated.");
+    }
 }
